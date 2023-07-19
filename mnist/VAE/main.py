6b5aa595c9758442476379da78097068377b767a
Maksym Sevkovych <m.sevkovych@duckeneers.de>
2023-07-19 14:44:39 +0200

    CODING: added seed, plot a single reconstruction

--- mnist/VAE/main.py
+++ mnist/VAE/main.py
@@ -1,23 +1,26 @@
+from random import seed
+
 import torch
-from modules import LinearVariationalAutoencoder
+from modules import ConvolutionalVariationalAutoencoder
 from torch.utils.data import DataLoader
 from torchvision import datasets, transforms
 
-from visualisations import plot_latent_2D_linear
+from visualisations import plot_latent_3D_convolutional, plot_reconstructed_for_point
 
 if __name__ == "__main__":
     # config
-    latent_dims = 2
-    num_epochs = 10
+    latent_dims = 3
+    num_epochs = 40
 
+    seed(0)
     # data
     transform = transforms.ToTensor()
     data = datasets.MNIST(root="./data", download=True, train=True, transform=transform)
     data_loader = DataLoader(dataset=data, batch_size=64, shuffle=True)
 
-    vae = LinearVariationalAutoencoder(latent_dims)
+    vae = ConvolutionalVariationalAutoencoder(latent_dims)
     with open(
-        f"./master-thesis/mnist/VAE/linear_vae_{num_epochs}_epochs_{latent_dims}_dims.pt",
+        f"./master-thesis/mnist/VAE/conv_vae_{num_epochs}_epochs_{latent_dims}_dims.pt",
         "rb",
     ) as f:
         vae.load_state_dict(torch.load(f))
@@ -25,7 +28,10 @@ if __name__ == "__main__":
     # print(
     #     f"VAE parameters: mu: {vae.encoder.mu.detach()}, sigma: {vae.encoder.sigma.detach()}" # noqa: E501
     # )
-    plot_latent_2D_linear(vae, data_loader, num_batches=50)
-    # plot_latent3D(vae, data_loader, 50)
+    # plot_latent_2D_convolutional(vae, data_loader, num_batches=50)
+    plot_latent_3D_convolutional(vae, data_loader, 50)
     # plot_latent3D_single_point(vae, data_loader)
-    # plot_reconstructed_2D(vae, (-3, 3), (-3, 3))
+    # plot_reconstructed_2D(vae, (-1.5, 1.5), (-1.5, 1.5))
+
+    point = torch.tensor([[1, -1, 0]], dtype=torch.float)
+    plot_reconstructed_for_point(vae, point)
